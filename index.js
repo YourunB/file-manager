b0304119761ede21213d1feacc17c80ee5ab7505
yourunb <bxyura@gmail.com>
2024-10-11 00:29:22 +0300

    feat: add command up

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
-const fs = require('fs');
-const path = require('path');
-const os = require('os');
-const { createHash, createReadStream, createWriteStream } = require('crypto');
-const { pipeline } = require('stream');
-const { promisify } = require('util');
-const { createBrotliCompress, createBrotliDecompress } = require('zlib');
+const fileSystem = require('fs');
+const filePath = require('path');
+const operatingSystem = require('os');
 
-const asyncPipeline = promisify(pipeline);
+const userHomeDir = operatingSystem.homedir();
+let currentWorkingDir = userHomeDir;
+let currentUsername = '';
 
-const homeDir = os.homedir();
-let currentDir = homeDir;
-let username = '';
-
-const displayCurrentDir = () => {
-  console.log(`You are currently in ${currentDir}`);
+const displayCurrentDirectory = () => {
+  console.log(`You are currently in ${currentWorkingDir}`);
 };
 
-const handleInput = async (input) => {
+const handleUserInput = async (input) => {
   const [command, ...args] = input.trim().split(' ');
 
+  switch (command) {
+    case 'up':
+      const parentDirectory = filePath.dirname(currentWorkingDir);
+      if (parentDirectory !== currentWorkingDir) currentWorkingDir = parentDirectory;
+      break;
 
-  displayCurrentDir();
+    default:
+      console.log('Invalid input');
+  }
+
+  displayCurrentDirectory();
 };
 
 const init = () => {
   const args = process.argv.slice(2);
 
-  const usernameArg = args.find(arg => arg.startsWith('--username='));
-  if (usernameArg) username = usernameArg.split('=')[1].trim();
-  else {
+  const usernameArgument = args.find(arg => arg.startsWith('--username='));
+  if (usernameArgument) {
+    currentUsername = usernameArgument.split('=')[1].trim();
+  } else {
     console.error('Error: Please provide a username using --username=your_username');
     process.exit(1);
   }
-  
-  console.log(`Welcome to the File Manager, ${username}!`);
-  displayCurrentDir();
+
+  console.log(`Welcome to the File Manager, ${currentUsername}!`);
+  displayCurrentDirectory();
 
   process.stdin.on('data', (data) => {
-    handleInput(data.toString());
+    handleUserInput(data.toString());
   });
 };
 
